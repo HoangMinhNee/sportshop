7d502a9bbbacbf3286261a266581e3afe6b78ec4
Hoang Minh <hoangminhh.281102@gmail.com>
2024-03-13 15:33:03 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sportshop/app.dart';
+import 'package:sportshop/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) => Get.put(AuthenticationRepository));
+
   runApp(const App());
 }
